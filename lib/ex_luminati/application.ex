7cd238d54450b8df10d82aa60da7a7fbaee62c41
Kamil Lelonek <squixy.sln@gmail.com>
2017-10-19 18:59:29 +0200

    Provide Application supervisor

---
+++ lib/ex_luminati/application.ex
@@ -0,0 +1,19 @@
+defmodule ExLuminati.Application do
+  use Application
+
+  def start(_type, _args),
+    do: Supervisor.start_link(children(), opts())
+
+  defp children do
+    [
+      ExLuminati.Agent,
+    ]
+  end
+
+  defp opts do
+    [
+      strategy: :one_for_one,
+      name:     ExLuminati.Supervisor,
+    ]
+  end
+end
--- mix.exs
+++ mix.exs
@@ -7,15 +7,16 @@ defmodule ExLuminati.Mixfile do
       version:         "0.1.0",
       elixir:          "~> 1.5",
       start_permanent: Mix.env() == :prod,
+      deps:            deps(),
       description:     description(),
       package:         package(),
-      deps:            deps(),
     ]
   end
 
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod:                {ExLuminati.Application, []},
     ]
   end
 
