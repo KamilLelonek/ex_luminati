c89267d1111453af513286efc14db3b9e66d052a
Kamil Lelonek <squixy.sln@gmail.com>
2017-10-20 13:46:50 +0200

    Remove test config

--- config/config.exs
+++ config/config.exs
@@ -3,8 +3,8 @@ use Mix.Config
 config :ex_luminati,
   proxy_host:     "zproxy.luminati.io",
   proxy_port:     22225,
-  proxy_username: "lum-customer-crowdmobile-zone-residential",
-  proxy_password: "358e1616accd",
+  proxy_username: "",
+  proxy_password: "",
   proxy_adapter:  HTTPoison,
   pool_size:      100
 
